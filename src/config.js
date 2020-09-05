12b21484023b7b9780623d3afeae91e290190c23
Will Olson <will.olson11@gmail.com>
2020-09-04 21:46:37 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploads-cyberhoodie"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://ycwx8uvub4.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_UxUENaOkv",
+    APP_CLIENT_ID: "1nt9d2pn98arbm951bmcue5u7g",
+    IDENTITY_POOL_ID: "us-east-1:159f1920-bb8f-489a-a6f0-b540aab0d391"
+  }
+};
