75689700777472d24307c2a3a870577d0aecf6ab
Will Olson <will.olson11@gmail.com>
2020-09-07 09:46:09 -0700

    Adding a Netlify build script

--- src/config.js
+++ src/config.js
@@ -1,18 +1,46 @@
-export default {
+const dev = {
   STRIPE_KEY: "pk_test_51HNUNSKbNQwzxjHWF1NQzbAfuyxplHHbGYvPelvoa9cNZ7V6iT0PHRF8PVUQ57PrRKzIC161BqwwcwNSWQiBPz0P000kpafb8w",
-  MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: "us-east-1",
-    BUCKET: "notes-app-uploads-cyberhoodie"
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-1f4p2909otxje"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://ycwx8uvub4.execute-api.us-east-1.amazonaws.com/prod"
+    URL: "api-dev.hackerform.com"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_UxUENaOkv",
-    APP_CLIENT_ID: "1nt9d2pn98arbm951bmcue5u7g",
-    IDENTITY_POOL_ID: "us-east-1:159f1920-bb8f-489a-a6f0-b540aab0d391"
+    USER_POOL_ID: "us-east-1_UAwTnD2C6",
+    APP_CLIENT_ID: "us-east-1_UAwTnD2C6",
+    IDENTITY_POOL_ID: "us-east-1:7537042a-ec29-466c-a56b-0ce5da9bf514"
   }
+};
+
+const prod = {
+  STRIPE_KEY: "pk_test_51HNUNSKbNQwzxjHWF1NQzbAfuyxplHHbGYvPelvoa9cNZ7V6iT0PHRF8PVUQ57PrRKzIC161BqwwcwNSWQiBPz0P000kpafb8w",
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-2-api-prod-attachmentsbucket-1gb7hhckyjsmv"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "api.hackerform.com"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_PY7iIBC3b",
+    APP_CLIENT_ID: "prnhrnv9lbrm16k14pt0ihokg",
+    IDENTITY_POOL_ID: "us-east-1:663dde52-d82c-4db4-8099-32aa4eb84487"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };
